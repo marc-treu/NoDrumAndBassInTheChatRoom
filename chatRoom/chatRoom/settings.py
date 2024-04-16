e422485a0753235673819593cb10110b3db4c0b2
marc-treu <mr.treu@gmail.com>
2024-04-16 23:12:58 +0200

    add all apps to INSTALLED_APPS

--- chatRoom/chatRoom/settings.py
+++ chatRoom/chatRoom/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "lobby.apps.LobbyConfig",
+    "lobby",
+    "ticTacToe",
+    "timeBomb",
+    "traitorsAboard",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- chatRoom/ticTacToe/apps.py
+++ chatRoom/ticTacToe/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TictactoeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tictactoe'
+    name = 'ticTacToe'
