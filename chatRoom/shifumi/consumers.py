8ba43064d818575de5b1a2e39a26e06be9bd740e
marc-treu <mr.treu@gmail.com>
2024-05-02 23:40:28 +0200

    fix webchat

--- chatRoom/shifumi/consumers.py
+++ chatRoom/shifumi/consumers.py
@@ -1,9 +1,10 @@
 import json
 import uuid
 
+
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 
 
 class ShifumiConsumer(WebsocketConsumer):
@@ -15,6 +16,7 @@ class ShifumiConsumer(WebsocketConsumer):
         self.player_id = str(uuid.uuid4())
         self.accept()
 
+        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.game_group_name, self.channel_name
         )
@@ -24,8 +26,12 @@ class ShifumiConsumer(WebsocketConsumer):
         )
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.game_group_name, self.channel_name
+        )
 
+    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
@@ -36,8 +42,8 @@ class ShifumiConsumer(WebsocketConsumer):
         self.msg.append({"message": message, "username": self.player_id})
 
         # Send message to room group
-        self.channel_layer.group_send(
-            self.game_group_name, {"type": "chat_message", "message": message, "username": self.player_id}
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_group_name, {"type": "chat.message", "message": message}
         )
 
     # Receive message from room group
