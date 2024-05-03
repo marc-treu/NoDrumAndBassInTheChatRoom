a87e8a6cd125abf4351f4c0c1cefb51c35814619
marc-treu <mr.treu@gmail.com>
2024-05-03 21:37:27 +0200

    handle chat and game in two socket

--- chatRoom/shifumi/consumers.py
+++ chatRoom/shifumi/consumers.py
@@ -4,7 +4,42 @@ import uuid
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from channels.layers import get_channel_layer
+
+GAME_GROUP_NAME = "game_group"
+
+
+class ChatConsumer(WebsocketConsumer):
+
+    def connect(self):
+        self.accept()
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            GAME_GROUP_NAME, self.channel_name
+        )
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            GAME_GROUP_NAME, self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data, **kwargs):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            GAME_GROUP_NAME, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({"message": message, "user": event.get("user")}))
 
 
 class ShifumiConsumer(WebsocketConsumer):
@@ -18,7 +53,7 @@ class ShifumiConsumer(WebsocketConsumer):
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
-            self.game_group_name, self.channel_name
+            GAME_GROUP_NAME, self.channel_name
         )
 
         # self.send(
@@ -28,41 +63,18 @@ class ShifumiConsumer(WebsocketConsumer):
     def disconnect(self, close_code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.game_group_name, self.channel_name
+            GAME_GROUP_NAME, self.channel_name
         )
 
     # Receive message from WebSocket
     def receive(self, text_data, **kwargs):
         text_data_json = json.loads(text_data)
-        action = text_data_json.get("action")
-        if action == "send_message":
-            self.handle_message(text_data_json)
-        elif action == "play":
-            self.handle_play(text_data_json)
-
-    def handle_message(self, json):
-        message = json["message"]
-        print(self.player_id, " envoie le msg suivant:", message)
-
-        # Store the message in cache
-        self.msg.append({"message": message, "username": self.player_id})
-
-        # Send message to room group
+        play = text_data_json["play"]
         async_to_sync(self.channel_layer.group_send)(
-            self.game_group_name, {"type": "chat.message", "message": message}
-        )
-
-    def handle_play(self, json):
-        play = json["play"]
-        async_to_sync(self.channel_layer.group_send)(
-            self.game_group_name, {"type": "chat.message", "message": f"{self.player_id} has play {play}", "user": json["user"]}
+            GAME_GROUP_NAME, {"type": "chat.message", "message": f"{self.player_id} has play {play}", "user": text_data_json["user"]}
         )
 
     # Receive message from room group
     def chat_message(self, event):
-
-        message = event["message"]
-        print(self.player_id, " Recoie le msg suivant:", message)
-
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message, "user": event.get("user")}))
+        self.send(text_data=json.dumps({"message": event.get("message"), "user": event.get("user")}))
--- chatRoom/shifumi/routing.py
+++ chatRoom/shifumi/routing.py
@@ -4,5 +4,6 @@ from . import consumers
 
 
 websocket_urlpatterns = [
+    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/shifumi/$', consumers.ShifumiConsumer.as_asgi()),
 ]
