485897dcb90f2df0ce9c0211dcd04da99e6df916
marc-treu <mr.treu@gmail.com>
2024-05-03 23:10:03 +0200

    separate js from html code

---
+++ chatRoom/static/js/shifumi.js
@@ -0,0 +1,64 @@
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+    if (e.key === 'Enter') {  // enter, return
+        document.querySelector('#chat-message-submit').click();
+    }
+};
+
+const roomName = JSON.parse(document.getElementById('room-name').textContent);
+let user = (Math.random() + 1).toString(36).substring(7);
+console.log("random", user);
+
+const chatSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/chat/'
+);
+
+const gameSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/shifumi/'
+);
+
+chatSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    document.querySelector('#chat-log').scrollTop = document.querySelector('#chat-log').scrollHeight;
+    document.querySelector('#chat-log').value += (data.message + '\n');
+};
+
+chatSocket.onclose = function(e) {
+    console.error('Chat socket closed unexpectedly');
+};
+
+gameSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    if (data.user !== user) {
+        document.querySelectorAll("button").forEach(e => {e.disabled=false});
+    }
+};
+
+gameSocket.onclose = function(e) {
+    console.error('Game socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-submit').onclick = function(e) {
+    const messageInputDom = document.querySelector('#chat-message-input');
+    const message = messageInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'user': user,
+        'message': message
+    }));
+    messageInputDom.value = '';
+};
+
+document.querySelectorAll('button').forEach(button => {
+    button.onclick = () => {
+        document.querySelectorAll("button").forEach(e => {e.disabled=true});
+        gameSocket.send(JSON.stringify({
+            'event': 'PLAY',
+            'user': user,
+            'play': button.innerHTML
+        }));
+    }
+});
